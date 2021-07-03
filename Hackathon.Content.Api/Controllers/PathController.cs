685581fbcaefebc222de6be4f748cd925bd079a9
Orest Tkachuk <orcoss36@gmail.com>
2021-07-03 10:44:48 +0300

    Add project files.

---
+++ Hackathon.Content.Api/Controllers/PathController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.Content.Api.Controllers
+{    
+    [ApiController]
+    public class PathController : ControllerBase
+    {
+        [HttpGet("/users/{userId:int}/videos/{videoId:int}/paths")]
+        public ActionResult GetUserVideoPaths(string priority)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Hackathon.Content.Api/Controllers/VideoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.Content.Api.Controllers
+{
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        [HttpGet("/users/{userId:int}/videos")]
+        public ActionResult GetUserVideos(string priority)
+        {
+            return Ok();
+        }
+    }
+}
